12bcdd34f65d67dda237d0775ff87f357f1762cd
Max Ayotte <maxayotte834@gmail.com>
2021-02-10 11:03:25 -0500

    New event form as well as post setup to persist new events to database

---
+++ client/src/components/EventForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import {Redirect} from 'react-router-dom'
+import ErrorList from './ErrorList'
+import translateServerErrors from './../services/translateServerErrors'
+
+const EventForm = (props) => {
+  const [errors, setErrors] = useState([])
+  const [shouldRedirect, setShouldRedirect]= useState(false)
+
+  const [newEventId, setNewEventId] = useState([])
+  const [newEvent, setNewEvent] = useState({
+    name: '',
+    date: ''
+  })
+
+  const addNewEvent = async () => {
+    try {
+      const response = await fetch('/api/v1/events', {
+        method: 'POST',
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(newEvent)
+      })
+      if (!response.ok) {
+        if(response.status === 422) {
+          const body = await response.json()
+          const newErrors = translateServerErrors(body.errors)
+          return setErrors(newErrors)
+        } else {
+          const errorMessage = `${response.status} (${response.statusText})`
+          const error = new Error(errorMessage)
+          throw(error)
+        }
+      } else {
+        const body = await response.json()
+        const newEvent = body.event
+        setNewEventId(newEvent.id)
+        if(body.event) {
+          setShouldRedirect(true)
+        }
+      }
+    } catch(error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
+
+  const handleInputChange = event => {
+    setNewEvent({
+      ...newEvent,
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addNewEvent(newEvent)
+    clearForm()
+  }
+
+  const clearForm = () => {
+    setNewEvent({
+      name: '',
+      date: ''
+    })
+  }
+
+  if (shouldRedirect){
+    return <Redirect to={'/'} />
+  }
+
+  return (
+    <div>
+      <h1>Add a New Event</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input 
+              type='text'
+              name='name'
+              onChange={handleInputChange}
+              value={newEvent.name}
+            />
+          </label>
+
+          <label>
+            Date:
+            <input 
+              type='text'
+              name='date'
+              onChange={handleInputChange}
+              value={newEvent.date}
+            />
+          </label>
+
+          <input 
+            type='submit'
+            value='Submit'
+          />
+        </form>
+    </div>
+  )
+}
+
+export default EventForm
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,4 +1,6 @@
 // include all of your models here using CommonJS requires
 const User = require("./User.js")
+const Event = require('./Event.js')
 
 module.exports = {User};
+module.exports = {Event}
