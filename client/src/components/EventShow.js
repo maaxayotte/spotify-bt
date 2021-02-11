75e435ccaafa9186a56d9d4ddc87c875e3cdfbaf
Max Ayotte <maxayotte834@gmail.com>
2021-02-11 14:21:37 -0500

    Temporary pause point

--- client/src/components/EventShow.js
+++ client/src/components/EventShow.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import ErrorList from './ErrorList'
+
+import NewPlaylistForm from './NewPlaylistForm'
 
 const EventShow = (props) => {
+  const [errors, setErrors] =useState([])
+  const [event, setEvent ] = useState([]) 
+  const { id } = useParams()
+
+  const getEvent = async () => {
+    try {
+      const response = await fetch(`/api/v1/events/${id}`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`
+        const error = new Error(errorMessage);
+        throw (error)
+      }
+      const eventsBody = await response.json()
+      setEvent(eventsBody.event)
+    } catch (error) {
+      console.log(`Error in fetch: ${error.message}`)
+    }
+  }
+
+  useEffect (() => {
+    getEvent()
+  }, [])
+
+  let newPlaylistForm = () => {
+    return (
+      <NewPlaylistForm />
+    )
+  }
+
   return(
     <div>
-      <h1>test</h1>
+      <ErrorList errors={errors} />
+      <div>
+        <h1>{event.name}</h1>
+        <h2>{event.date}</h2>
+      </div>
+      <div>
+        {newPlaylistForm()}
+      </div>
     </div>
   )
 }
---
+++ client/src/components/NewPlaylistForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+
+const NewPlaylistForm = () => {
+  const [newPlaylist, setNewPlaylist] = useState({
+    playlistUri: ''
+  })
+  
+  const handleInputChange = event => {
+    setNewPlaylist({
+      ...newPlaylist,
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+
+  return (
+    <div>
+      <form>
+        <label>
+          Playlist URI:
+          <input 
+            className='input'
+            id='playlistUri'
+            type='text'
+            name='playlistUri'
+            value={newPlaylist.playlistUri}
+            onChange={handleInputChange}
+          />
+        </label>
+
+        <div>
+          <input
+            type='submit'
+            value='Submit'
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default NewPlaylistForm
